8a5cbcf9bf33e2a9e9314f8444992e5755be1578
vinoddotcom <vy638814@gmail.com>
2026-06-08 12:10:59 +0530

    Update favicon and app icons

--- app/app.vue
+++ app/app.vue
@@ -12,6 +12,12 @@
 useHead({
   htmlAttrs: {
     'data-theme': 'km-theme'
-  }
+  },
+  link: [
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'manifest', href: '/site.webmanifest' }
+  ]
 });
 </script>
